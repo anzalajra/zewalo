3a4337ad14509ae206f59a23f1175b32ccdcbdca
Anzala Ajra <dang.anzala@gmail.com>
2026-04-04 16:41:55 +0700

    fix: remove view:cache incompatible with Filament

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -49,7 +49,7 @@ php artisan filament:assets
 echo "Optimizing application..."
 php artisan config:cache
 php artisan route:cache
-php artisan view:cache
+# php artisan view:cache
 php artisan event:cache
 php artisan icons:cache || true
 
